197c9dfd13be31bfa6b03dc99b069d96affc2055
Sergiu Caraian <sergiu.caraian@gmail.com>
2017-02-07 02:45:17 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/nBody.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nBody.dir/main.cpp.o"
+  "nBody.pdb"
+  "nBody"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nBody.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
